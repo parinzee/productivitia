9f91e6f729fb9cee05d53588864390bbdb73829f
Parinthapat Pengpun <parinzee@protonmail.com>
2022-02-13 11:08:01 +0700

    Added fade-in page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
+import { motion } from "framer-motion";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <motion.div
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
 
 export default MyApp;
