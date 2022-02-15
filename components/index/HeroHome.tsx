268c6073930a9a4e94e3b66ea4ae0023f72167d0
Parinthapat Pengpun <parinzee@protonmail.com>
2022-02-16 00:59:05 +0700

    Adjust UI slightly

--- components/index/HeroHome.tsx
+++ components/index/HeroHome.tsx
@@ -61,14 +61,6 @@ export default function HeroHome(): JSX.Element {
                     Start Now for Free
                   </a>
                 </Link>
-                <div>
-                  <a
-                    className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
-                    href="#0"
-                  >
-                    Learn more
-                  </a>
-                </div>
               </div>
             </div>
           </div>
