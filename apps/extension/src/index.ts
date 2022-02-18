251dec89371020cb28ea85decdc72ac1ec0b6411
Parinthapat Pengpun <parinzee@protonmail.com>
2022-02-18 14:46:10 +0700

    Added needed files to create an extension

---
+++ apps/extension/src/index.ts
@@ -0,0 +1,6 @@
+/* 
+This file merely redirects to the startpage for now. 
+However, I may add service workers and etc in the future.
+*/
+
+window.location.href = "https://productivitia.netlify.app/app";
