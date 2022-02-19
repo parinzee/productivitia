0011149ba6983f584917bb389f3743fac75c6b7a
Parinthapat Pengpun <parinzee@protonmail.com>
2022-02-19 13:35:11 +0700

    Rewrote redirecting extension

--- apps/extension/src/index.ts
+++ apps/extension/src/index.ts
@@ -2,5 +2,16 @@
 This file merely redirects to the startpage for now. 
 However, I may add service workers and etc in the future.
 */
+import Browser from "webextension-polyfill";
 
-window.location.href = "https://productivitia.netlify.app/app";
+const START_URL = "https://productivitia.netlify.app/app";
+
+async function main(): Promise<void> {
+  const tab = await Browser.tabs.getCurrent();
+  Browser.tabs.update(tab.id, { url: START_URL });
+  Browser.history.deleteUrl({ url: START_URL });
+}
+
+main().catch(console.error);
+
+export {};
