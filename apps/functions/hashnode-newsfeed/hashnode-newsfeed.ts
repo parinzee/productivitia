4a72b8619f73d1a2311c84becc27e3e6d3363302
Parinthapat Pengpun <parinzee@protonmail.com>
2022-03-03 21:38:00 +0700

    Updated compatability of hashnode-newsfeed function

--- apps/functions/hashnode-newsfeed/hashnode-newsfeed.ts
+++ apps/functions/hashnode-newsfeed/hashnode-newsfeed.ts
@@ -1,42 +1,33 @@
-import { ApolloClient, gql, HttpLink, InMemoryCache } from "@apollo/client";
 import { Handler } from "@netlify/functions";
-import fetch from "cross-fetch";
+import axios from "axios";
 
 export const handler: Handler = async (event, context) => {
-  const client = new ApolloClient({
-    link: new HttpLink({ uri: "https://api.hashnode.com/", fetch }),
-    cache: new InMemoryCache(),
-  });
-
-  const resp = await client.query({
-    query: gql`
-      {
-        storiesFeed(type: FEATURED) {
-          title
-          slug
-          author {
-            name
-            blogHandle
-          }
-        }
-      }
-    `,
-  });
+  const resp = await axios.get(
+    "https://hashnode.com/api/post/get-top-posts?duration=7"
+  );
 
   // Reformat the data with only the top six new stories
-  const data = [...resp.data.storiesFeed].slice(0, 6).map((element) => {
-    return {
-      title: element.title,
-      author: element.author.name,
-      url: `https://${element.author.blogHandle}.hashnode.dev/${element.slug}`,
-    };
+  const data = resp.data.posts.map((element: any) => {
+    if (element.publication.domain) {
+      return {
+        title: element.title,
+        author: element.author.name,
+        url: `https://${element.publication.domain}/${element.slug}`,
+      };
+    } else {
+      return {
+        title: element.title,
+        author: element.author.name,
+        url: `https://${element.publication.username}.hashnode.dev/${element.slug}`,
+      };
+    }
   });
 
   return {
     statusCode: 200,
     body: JSON.stringify(data),
     headers: {
-      "Cache-Control": "public, max-age=3600, must-revalidate",
+      "Cache-Control": "public, max-age=1800, must-revalidate",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type",
     },
